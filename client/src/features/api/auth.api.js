ec3ba820632f1719df300d1f1ec56780159f4109
MuriithiMark <markmwangi14317@gmail.com>
2024-03-16 12:37:37 +0300

    transitioned auth functions to rtk query

--- client/src/features/api/auth.api.js
+++ client/src/features/api/auth.api.js
@@ -10,6 +10,7 @@ export const loginUser = (builder) => builder
             method: "POST",
             body: loginData
         }),
+        providesTags: [{ type: "Auth", id: "login" }],
         transformResponse: (response) => response.user,
         transformErrorResponse: (response) => response.data,
     })
@@ -25,6 +26,42 @@ export const registerUser = (builder) => builder
             method: "POST",
             body: registerData,
         }),
+        providesTags: [{ type: "Auth", id: "register" }],
         transformResponse: (response) => response,
         transformErrorResponse: (response) => response.data,
     })
+
+
+/**
+ * @param {import("../../types/builder-function.d").BuilderFunction} builder
+ */
+export const logoutUser = (builder) => builder
+    .query({
+        query: () => ({
+            url: '/auth/login',
+            method: "GET"
+        }),
+        transformErrorResponse: (response) => response.data,
+        providesTags: [{ type: "Auth", id: "logout" }],
+        invalidatesTags: ["Auth", "Users", "Messages"]
+    })
+
+/**
+ * 
+ * @param {import("../../types/builder-function.d").BuilderFunction} builder 
+ */
+export const verifyToken = (builder) => builder
+    .query({
+        query: () => ({
+            url: '/auth/verify-token',
+            method: "GET",
+        }),
+        providesTags: [{ type: "Auth", id: "verify-token" }],
+        invalidatesTags: (response, error) => {
+            if (error) {
+                return ["Auth", "Users", "Messages"]
+            }
+        }
+    })
+
+// TODO idea to ask user to login/ re-verify their token after around 58 minutes
--- client/src/features/api/index.js
+++ client/src/features/api/index.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { SERVER_URL } from "../../utils/constants";
-import { loginUser, registerUser } from "./auth.api";
+import { loginUser, logoutUser, registerUser, verifyToken } from "./auth.api";
 import { getAllUsers, getUserById } from "./users.api";
 
 
@@ -9,8 +9,12 @@ const Api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: SERVER_URL, credentials: "include" }),
     tagTypes: ["Auth", "Users", "Messages"],
     endpoints: (builder) => ({
+        // Auth
         loginUser: loginUser(builder),
         registerUser: registerUser(builder),
+        verifyToken: verifyToken(builder),
+        logoutUser: logoutUser(builder),
+
         // Users
         getAllUsers: getAllUsers(builder),
         getUserById: getUserById(builder),
