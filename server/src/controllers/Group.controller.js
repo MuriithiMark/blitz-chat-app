89879b7798b9deb2fd7b4b01b25cc828b61c1ff7
MuriithiMark <markmwangi14317@gmail.com>
2024-03-21 03:23:54 +0300

    added group controller

--- server/src/controllers/Group.controller.js
+++ server/src/controllers/Group.controller.js
@@ -1,12 +1,22 @@
 import GroupModel from "../models/Group.model.js"
+import prisma from "../prisma.js";
 
 /**
  * @type {import("../type-definitions.d").ExpressFunction}
  */
 const createGroup = async (req, res) => {
-    const data = req.data;
+    const data = req.body;
     try {
-        const group = await GroupModel.create(data)
+        const groupData = {
+            ...data,
+            members: {
+                create: {
+                    memberId: req.session.user.id
+                }
+            }
+        }
+        console.log({ groupData })
+        const group = await GroupModel.create(groupData)
         res.status(200).send({ status: 'fail', group }).end()
     } catch (error) {
         res.status(500).send({ status: 'fail', message: error.message }).end()
@@ -43,10 +53,127 @@ const getGroupById = async (req, res) => {
     }
 }
 
+/**@type {import("../type-definitions.d").ExpressFunction} */
+const getUserGroups = async (req, res, next) => {
+    try {
+        const userId = req.session.user.id;
+        const groups = await prisma.groupMember.findMany({
+            where: {
+                memberId: userId,
+            },
+            include: {
+                group: true
+            }
+        })
+        const userGroups = groups.map((group) => group.group);
+        res.send({ status: 'success', groups: userGroups }).end()
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({ status: 'fail', message: error.message }).end()
+    }
+}
+
+/** @type {import("../type-definitions.d").ExpressFunction} */
+const addGroupMember = async (req, res, next) => {
+    try {
+
+        const groupId = req.params.groupId;
+        const memberId = req.body.memberId;
+
+        if (!groupId) {
+            return res.status(400).send({ status: 'fail', message: 'invalid group id' });
+        }
+
+        if (!memberId) {
+            return res.status(400).send({ status: 'fail', message: 'no new member id provided' });
+        }
+
+        const existingMember = await prisma.user.findUnique({
+            where: { id: memberId }
+        })
+        if (!existingMember) {
+            return res.status(400).send({ status: 'fail', message: 'member with given id does not exist' }).end()
+        }
+        const group = await prisma.groupMember.create({
+            data: {
+                role: "user",
+                group: {
+                    connect: {
+                        id: groupId
+                    }
+                },
+                member: {
+                    connect: {
+                        id: memberId
+                    }
+                }
+            },
+            include: {
+                group: true,
+                member: true
+            }
+        })
+        res.send({ status: 'success', group }).end()
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({ status: 'fail', message: error.message }).end()
+    }
+}
+
+
+const getGroupMembers = async (req, res, next) => {
+    try {
+        const groupId = req.params.groupId;
+
+        if (!groupId) {
+            return res.status(400).send({ status: 'fail', message: 'invalid group id' });
+        }
+
+        const group = await prisma.group.findUnique({
+            where: {
+                id: groupId
+            },
+            include: {
+                members: true
+            }
+        })
+        res.send({ status: 'success', group }).end()
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({ status: 'fail', message: error.message }).end()
+    }
+}
+
+const getGroupMessages = async (req, res, next) => {
+    try {
+        const groupId = req.params.groupId;
+
+        if (!groupId) {
+            return res.status(400).send({ status: 'fail', message: 'invalid group id' });
+        }
+        const group = await prisma.group.findUnique({
+            where: {
+                id: groupId
+            },
+            include: {
+                messages: true
+            }
+        })
+        res.send({ status: 'success', group }).end()
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({ status: 'fail', message: error.message }).end()
+    }
+}
+
 const GroupController = {
     createGroup,
     getGroupById,
-    getAllGroups
+    getAllGroups,
+    getUserGroups,
+    addGroupMember,
+    getGroupMembers,
+    getGroupMessages
 }
 
 export default GroupController;
--- server/src/routes/group-router.js
+++ server/src/routes/group-router.js
@@ -3,8 +3,16 @@ import GroupController from "../controllers/Group.controller.js";
 
 const groupRouter = Router();
 
-groupRouter.post("/new", GroupController.createGroup);
 groupRouter.get("/", GroupController.getAllGroups);
+groupRouter.post("/", GroupController.createGroup);
+
+groupRouter.get("/user", GroupController.getUserGroups);
+
 groupRouter.get("/:groupId", GroupController.getGroupById);
+
+groupRouter.post("/members/:groupId", GroupController.addGroupMember);
+groupRouter.get("/members/:groupId", GroupController.getGroupMembers);
+
+groupRouter.get("/messages/:groupId", GroupController.getGroupMessages);
 
 export default groupRouter;
