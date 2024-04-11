c5a3aeef54f22b4adfac4a3afd2992ced5d1fb16
MuriithiMark <markmwangi14317@gmail.com>
2024-04-11 17:51:28 +0300

    removed redundant client folder

--- client/src/assets/index.js
+++
@@ -1,8 +0,0 @@
-import UserProfileIcon from "./user-profile-icon.svg";
-import CircusGif from "./CircusGif_1.gif";
-import Spinner from "./Spinner-3.gif"
-export {
-    UserProfileIcon,
-    CircusGif,
-    Spinner
-}
--- client/src/types/builder-function.d.js
+++
@@ -1,5 +0,0 @@
-import { EndpointBuilder, BaseQueryFn, FetchArgs, FetchBaseQueryError, FetchBaseQueryMeta } from "@reduxjs/toolkit/query";
-
-/**
- * @typedef {EndpointBuilder<BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, {}, FetchBaseQueryMeta>, "Auth" | "Users" | "Friends" | "Messages", "api"> } BuilderFunction
- */
